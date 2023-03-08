e61eef232158c04ef49b6ce716ca7935efb2bfc5
Igor <dx.a@yandex.ru>
2023-03-08 17:49:55 +0300

    4

--- normal/Telegram_Projects/pythonProject15/imports.py
+++
@@ -1,8 +0,0 @@
-TOKEN = '5416667430:AAGiAixxU1UTyKFJNiDQhYrD0IWp2WyONuw'
-CHANNEL_URL = 'https://t.me/opopoq'
-CHANNEL_ID = '@opopoq'
-
-CHAT_ID = '@opopoq'  # CHAT_ID = '@testgroupforbotpy'
-ADMIN_ID = ['', '']  # 5195681649
-WORDS = ['блять', 'блядь', 'сука', 'ебать', 'ебанный', 'хуй', 'хуйло', 'хуесос', 'мат']
-WHITE_WORDS = ['https://www.google.com/']
